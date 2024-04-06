c924dd3cfe3302541db5940533accb2ae3a34cda
karina-kavaleuskaya <k.kavaleuskaya@mail.ru>
2024-04-06 22:14:45 +0300

    add-cart

--- shop_project/catalog/urls.py
+++ shop_project/catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from catalog.views import (CategoryListView, CategoryProductsView, SellerListView, SellerProductsView,
-                           DiscountListView, DiscountProductsView)
+                           DiscountListView, DiscountProductsView, CartView)
 
 urlpatterns = [
     path('categories/', CategoryListView.as_view(), name='categories'),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('sellers/', SellerListView.as_view(), name='sellers'),
     path('sellers/<int:seller_id>/', SellerProductsView.as_view(), name='sellers-product'),
     path('discounts/', DiscountListView.as_view(), name='discounts'),
-    path('discounts/<int:discount_id>/', DiscountProductsView.as_view(), name='discounts-product')
+    path('discounts/<int:discount_id>/', DiscountProductsView.as_view(), name='discounts-product'),
+    path('cart/', CartView.as_view(), name='cart')
 ]
